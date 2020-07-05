a2213ce6472451c949368919c0ba69f63a59e6e6
Christian Huitema <huitema@huitema.net>
2020-07-05 16:25:28 -0700

    Adding qloglist and qlogdown

---
+++ stats/qlogdown.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# This script loads a web page that indexes qlog files, and then downloads the individual qlogs
+
+import sys
+import requests
+import traceback
+import os.path
+
+# To test, choose a file containing a list of download url, and a folder
+
+if len(sys.argv) == 3:
+    url_list = sys.argv[1]
+    folder = sys.argv[2]
+else:
+    print("Usage: " + sys.argv[0] + " url-list folder")
+    exit(-1)
+
+downloaded = 0
+try:
+    # Using readlines() 
+    url_file = open(url_list, 'r') 
+    urls = url_file.readlines() 
+  
+    count = 0
+    # Strips the newline character 
+    for url in urls: 
+        # analyze the URL, extract the doc name
+        url = url.strip()
+        url_file_parts = url.split("/")
+        if len(url_file_parts) < 3:
+            print("No file name in " + url)
+        else:
+            file_name = url_file_parts[len(url_file_parts)-1]
+            file_path = folder + file_name
+            if os.path.isfile(file_path):
+                print("Skipping <" + url + "> as <" + file_path + "> exists.")
+            else:
+                try:
+                    r = requests.get(url) # create HTTP response object 
+                    qlog_text = r.content.decode('utf-8')
+                    try:
+                        file = open(file_path, 'w') 
+                        file.write(qlog_text)
+                        file.close()
+                        print("downloaded " + url + " as " + file_path + ", " + str(len(qlog_text)) + " bytes." )
+                        downloaded += 1
+                    except Exception as e:
+                        traceback.print_exc()
+                        print("Cannot save <" + file_path + ">, error: " + str(e));
+                        break
+                except Exception as e:
+                    traceback.print_exc()
+                    print("Cannot load <" + url + ">, error: " + str(e));
+                    break
+
+except Exception as e:
+    traceback.print_exc()
+    print("Cannot load <" + file_name + ">, error: " + str(e));
+
---
+++ stats/qloglist.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# This script loads a web page that indexes qlog files, and then downloads the individual qlogs
+
+import sys
+import requests
+import traceback
+
+
+# To test, choose a web site that provides qlog files
+
+if len(sys.argv) == 3:
+    qlog_index_url = sys.argv[1]
+    qlog_list_file = sys.argv[2]
+else:
+    print("Usage: " + sys.argv[0] + " qlog-server-url list_file")
+    exit(-1)
+
+# URL of the index  to be downloaded 
+r = requests.get(qlog_index_url) # create HTTP response object 
+tx = r.content.decode('utf-8')
+tx = tx.casefold()
+
+with open("qlog_index.txt",'w') as f1: 
+  
+    # Saving received content in text format 
+    
+    # write the contents of the response (r.content) 
+    # to a new file in binary mode. 
+
+    f1.write(tx)
+
+href_parts = tx.split('href=\"')
+print("Found " + str(len(href_parts)) + " href parts")
+all_url = []
+
+with open(qlog_list_file,'w') as f2:
+    for href_part in href_parts:
+        url_parts = href_part.split('\">')
+        url_dots = url_parts[0].split('.')
+        if len(url_dots) > 0 and url_dots[len(url_dots) - 1] == 'qlog':
+            qlog_url = url_parts[0]
+            if not qlog_url.startswith("http"):
+                qlog_url = qlog_index_url + qlog_url
+            all_url.append(qlog_url)
+            f2.write(qlog_url+"\n")
